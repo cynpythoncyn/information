0dc2100096fc5fc3ad64c3402f216c6a51fa9d83
cynpythoncyn <cyn1105690826@aliyun.com>
2019-03-03 23:23:08 +0800

    数据库迁移，导入静态文件，注册根路由以及网站的图标

--- info/modules/index/__init__.py
+++ info/modules/index/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+# 1.创建蓝图
 index_blu = Blueprint("index",__name__)
 
 from . import views
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,16 @@
 from . import index_blu
 
+from flask import render_template
+from flask import current_app
+
+
+# 2.使用蓝图
 
 @index_blu.route('/')
 def index():
-    return "hello world"
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
