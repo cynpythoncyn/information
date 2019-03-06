f02f6875f3c3819dc532bf24779227040ec19c54
cynpythoncyn <cyn1105690826@aliyun.com>
2019-03-06 19:52:58 +0800

    实现新闻列表数据展示，实现切换分类刷新逻辑，实现分页效果以及实现页面的滚动加载

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,7 @@
-
+from info.utils.response_code import RET
 from . import index_blu
 
-from flask import render_template, session
+from flask import render_template, session, request, jsonify
 from flask import current_app
 
 from info import constants
@@ -42,6 +42,8 @@ def index():
         categories_dicts.append(category.to_dict())
 
 
+
+
     data = {
         "user_info":user.to_dict() if user else None,
         "click_news_list":click_news_list,
@@ -53,3 +55,57 @@ def index():
 @index_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file('news/favicon.ico')
+
+
+@index_blu.route('/news_list')
+def get_news_list():
+    """
+    获取指定分类的新闻列表
+    1. 获取参数
+    2. 校验参数
+    3. 查询数据
+    4. 返回数据
+    :return:
+    """
+
+    # 1. 获取参数
+    args_dict = request.args
+    page = args_dict.get("page", '1')
+    per_page = args_dict.get("per_page", constants.HOME_PAGE_MAX_NEWS)
+    category_id = args_dict.get("cid", '1')
+
+    # 2. 校验参数
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 查询数据并分页
+    filters = []
+    # 如果分类id不为1，那么添加分类id的过滤
+    if category_id != "1":
+        filters.append(News.category_id == category_id)
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 获取查询出来的数据
+        items = paginate.items
+        # 获取到总页数
+        total_page = paginate.pages
+        current_page = paginate.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    news_li = []
+    for news in items:
+        news_li.append(news.to_basic_dict())
+    data = {
+        'news_dict_li':news_li,
+        'total_page':total_page,
+        'current_page':current_page,
+    }
+
+    # 4. 返回数据
+    return jsonify(errno=RET.OK, errmsg="OK", data=data)
