ae70d7bc7ac2f4b1961ecdc469e2b399da40f4af
cynpythoncyn <cyn1105690826@aliyun.com>
2019-03-06 13:38:14 +0800

    用户登陆逻辑前后端逻辑代码的编写以及首页右上角登陆和注册图标的显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,7 @@
+from info.models import User
 from . import index_blu
 
-from flask import render_template
+from flask import render_template, session
 from flask import current_app
 
 
@@ -8,7 +9,17 @@ from flask import current_app
 
 @index_blu.route('/')
 def index():
-    return render_template('news/index.html')
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    data = {
+        "user_info":user.to_dict() if user else None
+    }
+    return render_template('news/index.html',data=data)
 
 
 @index_blu.route('/favicon.ico')
--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,5 +1,6 @@
 import random
 import re
+from datetime import datetime
 
 from flask import current_app, jsonify, make_response, request, session
 
@@ -181,4 +182,52 @@ def register():
     session["mobile"] = user.mobile
 
     # 6. 返回注册结果
+    return jsonify(errno=RET.OK, errmsg="OK")
+
+@passport_blu.route('/login', methods=["POST"])
+def login():
+    """
+    1. 获取参数和判断是否有值
+    2. 从数据库查询出指定的用户
+    3. 校验密码
+    4. 保存用户登录状态
+    5. 返回结果
+    :return:
+    """
+
+    # 1. 获取参数和判断是否有值
+    json_data = request.json
+
+    mobile = json_data.get("mobile")
+    password = json_data.get("password")
+
+    if not all([mobile, password]):
+        # 参数不全
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+
+    # 2. 从数据库查询出指定的用户
+    try:
+        user = User.query.filter_by(mobile=mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据错误")
+
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg="用户不存在")
+
+    # 3. 校验密码
+    if not user.check_passowrd(password):
+        return jsonify(errno=RET.PWDERR, errmsg="密码错误")
+
+    # 4. 保存用户登录状态
+    session["user_id"] = user.id
+    session["nick_name"] = user.nick_name
+    session["mobile"] = user.mobile
+    # 记录用户最后一次登录时间
+    user.last_login = datetime.now()
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+    # 5. 登录成功
     return jsonify(errno=RET.OK, errmsg="OK")
