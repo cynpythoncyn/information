9e05073068db6bba9d6204ee090bbbed9d513279
cynpythoncyn <cyn1105690826@aliyun.com>
2019-03-06 18:47:25 +0800

    实现首页的分类展示功能

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -5,7 +5,8 @@ from flask import render_template, session
 from flask import current_app
 
 from info import constants
-from info.models import User, News
+from info.models import User, News, Category
+
 
 # 2.使用蓝图
 
@@ -31,10 +32,20 @@ def index():
         click_news_list.append(news.to_basic_dict())
 
 
+    # 获取新闻分类数据
+    categories = Category.query.all()
+    # 定义列表保存分类数据
+    categories_dicts = []
+
+    for category in categories:
+        # 拼接内容
+        categories_dicts.append(category.to_dict())
+
 
     data = {
         "user_info":user.to_dict() if user else None,
         "click_news_list":click_news_list,
+        "categories":categories_dicts,
     }
     return render_template('news/index.html',data=data)
 
